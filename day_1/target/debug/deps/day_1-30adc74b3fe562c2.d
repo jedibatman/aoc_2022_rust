6d444e4dfffc748bd458bb01db3478b88f20f8ad
Jared <jcsmith1859@gmail.com>
2022-12-12 09:24:04 -0600

    initial commit

---
+++ day_1/target/debug/deps/day_1-30adc74b3fe562c2.d
@@ -0,0 +1,5 @@
+/Users/jedibatman/repos/aoc_2022_rust/day_1/target/debug/deps/day_1-30adc74b3fe562c2.rmeta: src/main.rs
+
+/Users/jedibatman/repos/aoc_2022_rust/day_1/target/debug/deps/day_1-30adc74b3fe562c2.d: src/main.rs
+
+src/main.rs:
