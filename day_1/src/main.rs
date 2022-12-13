e8c4b1a45f8185ca907598408f69725918b629c2
Jared <jcsmith1859@gmail.com>
2022-12-13 11:57:46 -0600

    moving gitignore to root of repo, finishing day 1, and seeding day 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("src/input.txt")
+        .expect("Input file to be read");
+
+    let elf_loads: Vec<i32> =
+        contents
+            .split("\n\n")
+            .map(|s: &str| s.split_whitespace().map(|s: &str| s.parse::<i32>().unwrap()).sum())
+            .sorted()
+            .collect();
+
+    for (i, elf_load) in elf_loads.iter().enumerate() {
+        println!("elf {} caloric load: {}", i + 1, elf_load);
+    }
 }
