055cda4f905c91bf63e984c8405099b38c9be3a6
Jared <jcsmith1859@gmail.com>
2022-12-14 14:10:32 -0600

    removing target dir from day 1

--- day_1/target/debug/deps/day_1-6502bd7831952273.d
+++
@@ -1,5 +0,0 @@
-/Users/jedibatman/repos/aoc_2022_rust/day_1/target/debug/deps/day_1-6502bd7831952273.rmeta: src/main.rs
-
-/Users/jedibatman/repos/aoc_2022_rust/day_1/target/debug/deps/day_1-6502bd7831952273.d: src/main.rs
-
-src/main.rs:
